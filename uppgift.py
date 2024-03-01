337bc08cea51505d349e3f0222b1d8d51ebcba61
MrMysko <6714553+mrmysko@users.noreply.github.com>
2024-03-01 17:56:10 +0100

    Me caveman, recursion make brain hurt

--- uppgift.py
+++ uppgift.py
@@ -1,6 +1,33 @@
 # Skriv endast din funktionsdefinition här på denna indenteringsnivå! Det är
 # viktigt att du ger funktionen exakt det namn som står i beskrivningen.
 
+
+def treecoords(tree: dict, current_coord: tuple = ()) -> tuple:
+
+    # Recursion...complicated af
+
+    # Create an empty list to store tuples.
+    mod_list = []
+
+    # Iterate over the dict.
+    for key, value in tree.items():
+        # Check if the value is a nested dict.
+        if isinstance(value, dict):
+            # Recursion - Call on itself with the nested dict as argument, and key as cordinate.
+            test = treecoords(value, key)
+            print(type(test))
+            print(f"Recursed: {test}")
+        # If not, this is the end of the "chain", add the cordinates and value to the list.
+        else:
+            mod_list.append((key, value))
+
+    return mod_list
+
+    # OK, what is the base case?...we want to return a tuple with current_coords and the value
+    # {'a': 1} - lowest case. This should return as ('a',), 1)
+    # return ((key,), value)
+
+
 if __name__ == "__main__":
     # Här kan du skriva testkod för din funktion. Denna körs endast när du kör
     # filen direkt, och inte när du importerar den som modul i en annan fil.
@@ -12,4 +39,7 @@ if __name__ == "__main__":
     #
     # print(funktionsnamn("hejsan", 99))
     # print(funktionsnamn([19, 22, 31, 29, 1])
-    pass  # Ta bort denna rad
+    print(treecoords({"a": 1}))
+    print(treecoords({"a": 1, "b": 2}))
+    print(treecoords({"x": {"y": 3}, "z": 4}))
+    print(treecoords({"root": {"left": 5, "right": {"left": 6, "right": 7}}}))
