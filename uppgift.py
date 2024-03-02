227544570ff60e287ae20a3622da2b2d37892318
1banhime <thatdusse@gmail.com>
2024-03-02 14:57:08 +0100

    First commit

--- uppgift.py
+++ uppgift.py
@@ -1,15 +1,58 @@
-# Skriv endast din funktionsdefinition här på denna indenteringsnivå! Det är
-# viktigt att du ger funktionen exakt det namn som står i beskrivningen.
+def treecoords(tree: dict, current_coord: tuple=()) -> tuple:
+    '''Returns a tuple of tuples of where the values that aren't dictionaries are in a dictionary of dictionaries'''
+    results=[]
+    for key, value in tree.items():
+        current_coord = current_coord + (key, )
+        if isinstance(value, dict):
+            results.extend(treecoords(value, current_coord))
+        else:
+            results.append((current_coord, value))
+    return tuple(results)        
+            
+    
+    
 
 if __name__ == "__main__":
-    # Här kan du skriva testkod för din funktion. Denna körs endast när du kör
-    # filen direkt, och inte när du importerar den som modul i en annan fil.
-    # Koden importeras som en modul av autograding-funktionen för att utföra ett
-    # "smoke test" av din funktion, så det är viktigt att din kod inte kör något
-    # utanför denna if-sats.
-    #
-    # Exempel:
-    #
-    # print(funktionsnamn("hejsan", 99))
-    # print(funktionsnamn([19, 22, 31, 29, 1])
-    pass  # Ta bort denna rad
+    print(treecoords({
+    "a": 1,
+    "b": 2
+}))
+    print(treecoords({
+    "x": {
+        "y": 3
+    },
+    "z": 4
+}))
+    print(treecoords({
+    "root": {
+        "left": 5,
+        "right": {
+            "left": 6,
+            "right": 7
+        }
+    }
+}))
+    print(treecoords({
+    "1": {
+        "2": {
+            "3": {}
+        },
+        "4": {
+            "5": 8,
+            "6": 9
+        }
+    }
+}))
+    print(treecoords({
+    "a": {
+        "b": {
+            "c": 10,
+            "d": 11
+        },
+        "e": {
+            "f": 12
+        }
+    },
+    "g": 13
+}))
+    
