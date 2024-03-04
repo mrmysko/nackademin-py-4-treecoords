fed5839f49800ad8f856aafeca0998ec8aab8c7a
Andreas Brandting <andreas@brandting.com>
2024-03-04 10:32:41 +0100

    Inlämning 4

--- uppgift.py
+++ uppgift.py
@@ -1,15 +1,34 @@
-# Skriv endast din funktionsdefinition här på denna indenteringsnivå! Det är
-# viktigt att du ger funktionen exakt det namn som står i beskrivningen.
+def treecoords(tree: dict, current_coord: tuple = ()) -> tuple:
+
+    result = ()  # Skapar en tom tuple för att spara resultaten.
+
+    for key, value in tree.items():  # Loopar igenom varje nyckelpar i tree
+
+        if isinstance(
+            value, dict
+        ):  # Kontrollera om Value är en dictionary med hjälp av funktionen isinstance.
+
+            new_coord = current_coord + (
+                key,
+            )  # om påståendet är sant, så sparas nuvarande kordinaters nyckel i variablen new_coord
+
+            result += treecoords(
+                value, new_coord
+            )  # Använder recusion och tillkallar treecoords med variablarna 'value' och 'new_coord'.
+
+        else:
+
+            result += (
+                (current_coord + (key,), value),
+            )  # Om värdet till nyckeln inte är en dict, så sparas nuvarande kordinaterna, nyckeln och värdet till result.
+
+    return result  # Returnerar värdet av tupeln
+
 
 if __name__ == "__main__":
-    # Här kan du skriva testkod för din funktion. Denna körs endast när du kör
-    # filen direkt, och inte när du importerar den som modul i en annan fil.
-    # Koden importeras som en modul av autograding-funktionen för att utföra ett
-    # "smoke test" av din funktion, så det är viktigt att din kod inte kör något
-    # utanför denna if-sats.
-    #
-    # Exempel:
-    #
-    # print(funktionsnamn("hejsan", 99))
-    # print(funktionsnamn([19, 22, 31, 29, 1])
-    pass  # Ta bort denna rad
+    # Tillkallar funktionen med
+    treecoords({"a": 1, "b": 2})
+    treecoords({"x": {"y": 3}, "z": 4})
+    treecoords({"root": {"left": 5, "right": {"left": 6, "right": 7}}})
+    treecoords({"1": {"2": {"3": {}}, "4": {"5": 8, "6": 9}}})
+    treecoords({"a": {"b": {"c": 10, "d": 11}, "e": {"f": 12}}, "g": 13})
