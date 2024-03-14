7c46f1ab420d97fc1448a109caeb3063b6568d19
bjorn5132 <alexander.sawmi@yh.nackademin.se>
2024-03-14 14:34:52 +0100

    Hej

--- uppgift.py
+++ uppgift.py
@@ -1,21 +1,20 @@
-# Skriv endast definitioner här på denna indenteringsnivå! Det är viktigt att du
-# namnger funktioner, klasser och variabler exakt med de namn som står i
-# beskrivningen.
+def treecoords(tree):
+
+    result = []
+
+    for key, value in tree.items():
+        if isinstance(value, dict):
+            sub_coords = treecoords(value)
+            for coord, val in sub_coords:
+                result.append(((key,) + coord, val))
+        else:
+            result.append(((key,), value))
+
+    return tuple(result)
+
 
 if __name__ == "__main__":
-    # Här kan du skriva testkod som bara körs när du kör filen direkt och inte
-    # när den importeras som modul i en annan fil.
-    #
-    # Koden importeras som en modul av autograding-funktionen för att utföra ett
-    # "smoke test" av din funktion, så det är viktigt att din kod inte kör något
-    # utanför denna if-sats.
-    #
-    # Exempel:
-    #
-    # print(funktionsnamn("hejsan", 99))
-    # print(funktionsnamn([19, 22, 31, 29, 1])
-    #
-    # Exempel:
-    # minklass = Klass()
-    # print(klass.leet("hejsan")
-    pass  # Ta bort denna rad när du skriver din kod
+    print(treecoords({"kebab": 1, "kebabtallrik": 2}))
+    print(treecoords({"a": {"b": {"c": 10, "d": 11}, "e": {"f": 12}}, "g": 13}))
+
+    print(treecoords({"1": {"2": {"3": {}}, "4": {"5": 8, "6": 9}}}))
