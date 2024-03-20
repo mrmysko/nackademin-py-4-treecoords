0cac54acc5774dec2d367e719df194b25f72c70e
Usayd2 <usaydshikho@gmail.com>
2024-03-20 19:47:44 +0100

    Har skrivit en funktion som läser träd strukturerat data och omvanldar det till en platt lista av tuples.

--- uppgift.py
+++ uppgift.py
@@ -1,15 +1,62 @@
-# Skriv endast din funktionsdefinition här på denna indenteringsnivå! Det är
-# viktigt att du ger funktionen exakt det namn som står i beskrivningen.
+def treecoords(tree: dict, current_coord: tuple=()) -> tuple: 
+    # Funktionen treeecoords läser träd strukturerat data och omvanldar det till en platt lista av tuples. 
+    # Varje tuple har en sökväg i trädet.
+    results = []  # Skapar en tom lista för att samla ihop alla koordinaterna och värdena.
+
+    for key, value in tree.items(): # Loopar igenom varje element i trädet.
+        new_coord = current_coord + (key,) # Skapar en ny koordinat baserad på den aktuella platsen.
+
+        if isinstance(value, dict):
+            # Om värdet är en dictionary då finns det mer nivåer att utforska i trädet genom ett rekursivt anrop.
+            # Loopen fortsätter tills den når den sista nivån i trädet.
+            results.extend(treecoords(value, new_coord))
+        else: 
+            # Om värdet inte är en dictionary, läggs den nya koordinat och värdet i listan "results".
+            results.append((new_coord, value))
+
+    return tuple(results)  # Resultatet returneras som en tuple.
 
 if __name__ == "__main__":
-    # Här kan du skriva testkod för din funktion. Denna körs endast när du kör
-    # filen direkt, och inte när du importerar den som modul i en annan fil.
-    # Koden importeras som en modul av autograding-funktionen för att utföra ett
-    # "smoke test" av din funktion, så det är viktigt att din kod inte kör något
-    # utanför denna if-sats.
-    #
-    # Exempel:
-    #
-    # print(funktionsnamn("hejsan", 99))
-    # print(funktionsnamn([19, 22, 31, 29, 1])
-    pass  # Ta bort denna rad
+
+    print(treecoords({
+    "a": 1,
+    "b": 2
+}))
+    print(treecoords({
+    "x": {
+        "y": 3
+    },
+    "z": 4
+}))
+    print(treecoords({
+    "root": {
+        "left": 5,
+        "right": {
+            "left": 6,
+            "right": 7
+        }
+    }
+}))
+    print(treecoords({
+    "1": {
+        "2": {
+            "3": {}
+        },
+        "4": {
+            "5": 8,
+            "6": 9
+        }
+    }
+}))
+    print(treecoords({
+    "a": {
+        "b": {
+            "c": 10,
+            "d": 11
+        },
+        "e": {
+            "f": 12
+        }
+    },
+    "g": 13
+}))
