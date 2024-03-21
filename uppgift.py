2257064051d392a85b51c7215c283ff8512a2fc3
Nastaran <nastaran.bazaz@gmail.com>
2024-03-21 08:35:15 +0100

    inlämning 4

--- uppgift.py
+++ uppgift.py
@@ -7,37 +7,34 @@ def treecoords(tree: dict, current_coord: tuple = ()) -> tuple:
     for key, value in tree.items():
 
         # Uppdatera den aktuella koordinaten med den nuvarande nyckeln
-        current_coord = current_coord + (key,)
+        new_current_coord = current_coord + (key,)
+        # print("key:", (key,))
+        # print("new_current_coord:", new_current_coord)
 
         # Om värdet är en dictionary, gör ett rekursivt anrop
         if isinstance(value, dict):
-            # Anropa treecoords rekursivt och lagra resultatet i variabeln c
-            c = treecoords(value, current_coord)
-            # Lägg till c i resultatlistan
-            result.append(c)
-            # print(c)
+
+            result.extend(treecoords(value, new_current_coord))
+            # print("key:", (key,))
+            # print("new_current_coord:", new_current_coord)
+
         else:
-            # Om värdet inte är en dictionary, skapa en tuple med koordinaten och värdet
-            a = current_coord
-            # print(a)
-            b = a, value
-            b = tuple(b)
-            # Lägg till b i resultatlistan
-            result.append(b)
+
+            result.append((new_current_coord, value))
 
     # Konvertera listan med resultat till en tuple och returnera den
     return tuple(result)
 
 
-# # Exempel 1
-# result_1 = treecoords({"a": 1, "b": 2})
-# print(result_1)  # förvänta return ( (("a",), 1),(("b",), 2))
+# Exempel 1
+result_1 = treecoords({"a": 1, "b": 2})
+print(result_1)  # förvänta return ( (("a",), 1),(("b",), 2))
 
 # Exempel 2
 result_2 = treecoords({"x": {"y": 3}, "z": 4})
 print(result_2)  # ((("x", "y"), 3),(("z",), 4))
 
-# # Exempel 3
-# result_3 = treecoords({"root": {"left": 5, "right": {"left": 6, "right": 7}}})
-# print(result_3)
+# Exempel 3
+result_3 = treecoords({"root": {"left": 5, "right": {"left": 6, "right": 7}}})
+print(result_3)
 # # ((("root", "left"), 5),(("root", "right", "left"), 6),(("root", "right", "right"), 7))
