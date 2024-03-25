ddb1ae7c9ab646c3f0f70dddc3b0e5af71751f5a
Johan <johan.zetterholm@yh.nackademin.se>
2024-03-25 14:47:05 +0100

    Lektion4

--- uppgift.py
+++ uppgift.py
@@ -1,15 +1,42 @@
-# Skriv endast din funktionsdefinition här på denna indenteringsnivå! Det är
-# viktigt att du ger funktionen exakt det namn som står i beskrivningen.
+
+def treecoords(tree: dict, current_coord: tuple=()) -> tuple:
+    
+    tree_value = []
+    
+    for key, value in tree.items():
+        key_value = current_coord + (key, )
+        
+        if isinstance(value, dict):
+            tree_value.extend(treecoords(value, key_value))
+        else:
+            tree_value.append(tuple((key_value, value)))
+            
+            
+    return tuple(tree_value)
+        
 
 if __name__ == "__main__":
-    # Här kan du skriva testkod för din funktion. Denna körs endast när du kör
-    # filen direkt, och inte när du importerar den som modul i en annan fil.
-    # Koden importeras som en modul av autograding-funktionen för att utföra ett
-    # "smoke test" av din funktion, så det är viktigt att din kod inte kör något
-    # utanför denna if-sats.
+    
     #
     # Exempel:
+    print(treecoords({
+        "a": 1,
+        "b": 2
+    }))
+    #  
+    print(treecoords({
+    "root": {
+        "left": 5,
+        "right": {
+            "left": 6,
+            "right": 7
+            }
+        }
+    }))
     #
-    # print(funktionsnamn("hejsan", 99))
-    # print(funktionsnamn([19, 22, 31, 29, 1])
-    pass  # Ta bort denna rad
+    treecoords({
+    "x": {
+        "y": 3
+        },
+        "z": 4
+    })
